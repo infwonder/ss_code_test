6e60d49d8db6fa04e95465ebb2c8d0a60adac26d
Yu-Cheng Lin <infwonder@gmail.com>
2017-05-07 06:47:12 -0600

    retry with timeout by nodejs server

--- querys.js
+++ querys.js
@@ -12,11 +12,11 @@ module.exports =
 {
   bittrex: undefined,
   poloniex: undefined,
-  exchanges: undefined,
-  markets: undefined,
+  exchanges: 'Poloniex,Bittrex',
+  markets: 'BTC-DASH,BTC-LTC,BTC-ETH',
   init: false,
 
-  prepare: function(exstr, mktstr, callback) 
+  prepare: function(callback) 
   {
     if (module.exports.init) return callback();
 
@@ -24,8 +24,6 @@ module.exports =
       module.exports.poloniex = new Poloniex(undefined, undefined);
       module.exports.bittrex = bittrex;
       module.exports.bittrex.options({'baseUrl': 'https://bittrex.com/api/v1.1'});
-      module.exports.exchanges = exstr || 'Poloniex,Bittrex';
-      module.exports.markets   = mktstr || 'BTC-DASH,BTC-LTC,BTC-ETH';
       module.exports.init = true;
       callback();
     } catch(err) {
@@ -40,38 +38,44 @@ module.exports =
     if (exchange === 'Bittrex') {
       markets.map((i) => 
       {
-         module.exports.bittrex.getticker({ market: i}, (data) => 
+         module.exports.bittrex.getticker({market: i}, (data) => 
          {
            o[exchange][i] = data.result; 
-           if(Object.keys(o[exchange]).length === markets.length) callback(o);
+           if(Object.keys(o[exchange]).length === markets.length) callback(null, o);
          });
       });
     } else if (exchange === 'Poloniex') {
       module.exports.poloniex.returnTicker((err, data) => {
+        if (err) return callback(err, null);
+
         markets.map((i) => {
           var j = i.replace('-','_'); o[exchange][i] = {};
           o[exchange][i]['Bid'] = data[j]['highestBid'];
           o[exchange][i]['Ask'] = data[j]['lowestAsk'];
           o[exchange][i]['Last'] = data[j]['last'];
-          if (Object.keys(o[exchange]).length === markets.length) callback(o);
+          if (Object.keys(o[exchange]).length === markets.length) callback(null, o);
         });
       });
     } else {
-      throw('not supported');
+      callback('not supported', null);
     }
   },
 
-  update: function(callback)
+  update: function(mktstr, callback)
   {
+    module.exports.markets = mktstr;
+
     var m  = module.exports.markets.split(/,/);
     var c  = module.exports.exchanges.split(/,/).length;
     var iterator = Exchanges(module.exports.exchanges);
     var r = iterator.next();
-    var output = {}; var i = 0;
+    var output = {};
   
     while(!r.done) {
-      module.exports.chkMarkets(r.value, m, (out) => 
+      module.exports.chkMarkets(r.value, m, (err, out) => 
       {
+         if(err) return callback(err, null);
+
          Object.assign(output ,out);
          var k = Object.keys(output);
          if( k.length === c ) {
@@ -79,7 +83,7 @@ module.exports =
            m.map( (f) => {
              redo[f] = []
              k.map( (h) => { redo[f].push( [h, output[h][f]['Ask']] ) });
-             if (Object.keys(redo).length === m.length) callback(redo);
+             if (Object.keys(redo).length === m.length) callback(null, redo);
            });
          }
       });
@@ -87,35 +91,18 @@ module.exports =
     }
   },
 
-/*
-  update: function(market, callback)
+  compare: function(mktstr, callback)
   {
-    var c  = module.exports.exchanges.split(/,/).length;
-    var iterator = Exchanges(module.exports.exchanges);
-    var r = iterator.next();
-    var output = {}; 
-  
-    while(!r.done) {
-      module.exports.chkMarkets(r.value, [market], (out) => 
-      { 
-         Object.assign(output ,out);
-         if( Object.keys(output).length === c ) callback(output);
-      });
-      r = iterator.next();
-    }
-  },
-*/
-
-  compare: function(callback)
-  {
-    module.exports.update((results) =>
+    module.exports.markets = mktstr;
+    module.exports.update(mktstr, (err, results) =>
     {
+      if (err) return callback(err, null);
       //console.log(JSON.stringify(results, null, 2));
       var output = {};
       Object.keys(results).map( (r) =>
       {
         output[r] = results[r].sort(function(a,b) { return a[1] - b[1]});
-        if( Object.keys(output).length == Object.keys(results).length) callback(output);
+        if( Object.keys(output).length == Object.keys(results).length) callback(null, output);
       });
     });
   }
--- server.js
+++ server.js
@@ -1,9 +1,72 @@
-'use strict';
+const Promise = require('bluebird');
+const express = require('express');
+const http = require("http");
 
-// Poloniex uses WASP
-const autobahn = require('autobahn');
-const bittrexapi = require('node.bittrex.api');
+var bodyParser = require('body-parser');
+var handlebars = require("express-handlebars")
+      .create(
+        {
+           defaultLayout: 'main',
+        });
 
-// API URLs
-var poloWS = 'wss://api.poloniex.com';
-var btxAPI = 
+var app = express();
+app.engine('hds', handlebars.engine);
+app.set('view engine', 'hds');
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: true }));
+
+function catchedError(response, error)
+{
+  response.status(500);
+  response.render("500", {"error": error});
+}
+
+var query = require( __dirname + '/querys.js');
+var prepare = Promise.promisify(query.prepare);
+var update  = Promise.promisify(query.update);
+var compare = Promise.promisify(query.compare);
+var chkMarkets = Promise.promisify(query.chkMarkets);
+
+app.get('/', function(request, response, next)
+{
+  var watching = 'BTC-DASH,BTC-LTC,BTC-ETH,BTC-SJCX,BTC-GNT';
+  
+  prepare().then(() => {
+   // chkMarkets('Bittrex', ['BTC-SJCX','ETH-GNT']).then((results) => { console.log(JSON.stringify(results, null, 2)) });
+  
+    update(watching).then((results) => {
+      compare(watching).then((output) => {
+        var o = [];
+        Object.keys(output).map((i) => {
+/*
+           console.log("Market: " + i + ", Best choice: " + 
+                        output[i][0][0] + ", Price: " + 
+                        output[i][0][1] + ", Compared to: " + 
+                        output[i][1][0] + ", Price: " + 
+                        output[i][1][1]);
+*/
+           o.push({ "Market": i, 'Best' : output[i][0][0], [output[i][0][0]]: output[i][0][1], [output[i][1][0]]: output[i][1][1] });
+        });
+
+        return o;
+      })
+      .then((o) => 
+      {
+         response.render('answer', {list: o});
+         console.log(o);
+      }).catch((err) => { next(err);});
+    }).catch((err) => { next(err);});
+  }).catch((err) => { next(err);});
+});
+
+app.use(function(error, request, response, next) {
+  try {
+    setTimeout(() => { response.redirect(303, '/') }, 10000);
+  } catch(err) {
+    catchedError(response, err);
+  }
+});
+
+// Start it up!
+http.createServer(app).listen(8081);
+
